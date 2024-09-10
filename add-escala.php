c51bed6f209a94f639c315aba70f8a5af487b053
lini-code <karolininicolau@gmail.com>
2024-09-10 17:27:10 -0300

    projeto atualizado

---
+++ add-escala.php
@@ -0,0 +1,45 @@
+<?php
+// Configuração do banco de dados
+$servername = "localhost";  // Normalmente "localhost"
+$username = "root";         // Usuário do banco de dados
+$password = "";             // Senha do banco de dados
+$dbname = "healconect";     // Nome do banco de dados
+
+// Conectar ao banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+// Verificar se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $especialidade = $_POST['especialidade'];
+    
+    // Captura dos dias de atendimento selecionados
+    $dias_atendimento = [];
+    if (isset($_POST['segunda'])) $dias_atendimento[] = 'Segunda';
+    if (isset($_POST['terca'])) $dias_atendimento[] = 'Terça';
+    if (isset($_POST['quarta'])) $dias_atendimento[] = 'Quarta';
+    if (isset($_POST['quinta'])) $dias_atendimento[] = 'Quinta';
+    if (isset($_POST['sexta'])) $dias_atendimento[] = 'Sexta';
+    if (isset($_POST['sabado'])) $dias_atendimento[] = 'Sábado';
+    if (isset($_POST['domingo'])) $dias_atendimento[] = 'Domingo';
+
+    // Converter array de dias para string separada por vírgulas
+    $dias_atendimento_str = implode(', ', $dias_atendimento);
+
+    // Inserir dados na tabela
+    $sql = "INSERT INTO medicos (nome, especialidade, dias_atendimento) VALUES ('$nome', '$especialidade', '$dias_atendimento_str')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Novo médico adicionado com sucesso!";
+    } else {
+        echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
